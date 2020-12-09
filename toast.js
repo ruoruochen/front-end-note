622c5b0370d4e9fd97646ab2574c98ff1f78c9ca
ruoruochen <2679330388@qq.com>
2020-12-09 09:21:17 +0800

    no message

--- toast.js
+++
@@ -1,9 +0,0 @@
-const notifier = require("node-notifier");
-const path = require('path');
-
-module.exports = function(message) {
-    notifier.notify({
-        title: "nodejsServer消息",
-        message,
-    })
-}
